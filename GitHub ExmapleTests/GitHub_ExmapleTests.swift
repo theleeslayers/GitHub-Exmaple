a49c6228f8440a57252789b24bdeb0761710b54a
Aslan Lee <aslanlee@aslans-macbook-pro.local>
2018-07-06 10:35:16 -0500

    Initial Commit

---
+++ GitHub ExmapleTests/GitHub_ExmapleTests.swift
@@ -0,0 +1,36 @@
+//
+//  GitHub_ExmapleTests.swift
+//  GitHub ExmapleTests
+//
+//  Created by Aslan Lee on 7/6/18.
+//  Copyright © 2018 Aslan Lee. All rights reserved.
+//
+
+import XCTest
+@testable import GitHub_Exmaple
+
+class GitHub_ExmapleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
